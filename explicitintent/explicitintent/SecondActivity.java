4c975588894ed455a7ce9fdfa5e3309e42f251c4
Shraddhadhumal-31 <shraddhadhumal31@gmail.com>
2026-05-21 23:09:13 +0530

    Update SecondActivity.java

--- explicitintent/explicitintent/SecondActivity.java
+++ explicitintent/explicitintent/SecondActivity.java
@@ -1,14 +1,32 @@
 package com.example.explicitintent;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    TextView tvResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        tvResult = findViewById(R.id.tvResult);
+
+        // Receiving data
+        String name = getIntent().getStringExtra("studentName");
+        String course = getIntent().getStringExtra("studentCourse");
+        String marks = getIntent().getStringExtra("studentMarks");
+
+        String result =
+                "Student Details\n\n" +
+                        "Name : " + name + "\n" +
+                        "Course : " + course + "\n" +
+                        "Marks : " + marks;
+
+        tvResult.setText(result);
     }
 }
